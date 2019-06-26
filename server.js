850b077afedcb195a98dbfd2cc9e38a04e79e9b1
mjalbert13 <mjalbert13@gmail.com>
2019-06-26 16:26:20 -0400

    set up server

--- server.js
+++ server.js
@@ -1,2 +1,23 @@
 var express = require("express");
 
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+var routs = require("./controlers/burger_controler.js");
+
+app.use(routs);
+
+app.listen(PORT, function(){
+    console.log("Server is listening on localhost:"+PORT);
+})
